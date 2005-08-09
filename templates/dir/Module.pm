095fb6c83ad51cd6ea04a91f2ad27403f7ff5875
Ricardo SIGNES <rjbs@codesimply.com>
2005-08-09 02:49:35 +0000

    initial import

---
+++ templates/dir/Module.pm
@@ -0,0 +1,70 @@
+package [%module%];
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+[%module%] - The fantastic new [%module%]!
+
+=head1 Version
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 Synopsis
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use [%module%];
+
+    my $foo = [%module%]->new;
+    ...
+
+=head1 Export
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 Functions
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 Author
+
+[%self.author%], C<< <[%self.email%]> >>
+
+=head1 Bugs
+
+Please report any bugs or feature requests to
+C<bug-[%rtname%]@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.  I will be notified, and then you'll automatically be
+notified of progress on your bug as I make changes.
+
+=head1 Copyright & License
+
+Copyright [%year%] [%self.author%], All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1; # End of [%module%]
